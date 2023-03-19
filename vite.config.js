8577cfdb16186293a3e5fd05b31757dde7878cbd
Георгий Петренко <58736243+Georgy87@users.noreply.github.com>
2023-03-20 01:52:27 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vite-project-exemple/',
+  base: '/CHAT_EE/',
   plugins: [react()],
 })
